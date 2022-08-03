163d8ca65586be960c1e032297399f375ea13b6e
Muhammad Talha <talhakhalid411@gmail.com>
2022-08-03 13:03:45 +0500

    work

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,11 +4,12 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 type Data = {
   name: string
   work: string
+  bug: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: `${process.env.VERCEL_URL}`, work: `${process.env.VERCEL_ENV}` })
+  res.status(200).json({ name: `${process.env.VERCEL_URL}`, work: `${process.env.VERCEL_ENV}`, bug:`${process.env.FAUNA_SECRET}` })
 }
