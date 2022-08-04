4890558f35c22b478b9ce52651fb4441e4ad7ef5
Muhammad Talha <talhakhalid411@gmail.com>
2022-08-04 12:18:54 +0500

    path changes

--- pages/api/apollo.ts
+++ pages/api/apollo.ts
@@ -17,9 +17,9 @@ export default cors(async function handler(req:any,res:any){
     await startServer;
 
     await server.createHandler({
-        // path: 'https://v-lollipop.vercel.app/api/apollo',
+        path: 'https://v-lollipop.vercel.app/api/apollo',
         // path: `${process.env.VERCEL_URL}/api/apollo`,
-        path: '/api/apollo',
+        // path: '/api/apollo',
         // path: `${process.env.NEXT_PUBLIC_MY_URL}/api/apollo`,
         // path: `${process.env.VERCEL_URL}/api/apollo`,
     })(req,res);
